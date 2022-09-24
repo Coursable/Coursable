663ba936dab0f6f2bb159c1d772bd7eb4b0da5e3
GoddlyGut <a.reitman@icloud.com>
2022-09-23 20:33:32 -0400

    Initial Commit

---
+++ SchoolSchedule/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SchoolSchedule
+//
+//  Created by Ari Reitman on 9/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SchoolSchedule/SchoolScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolScheduleApp.swift
+//  SchoolSchedule
+//
+//  Created by Ari Reitman on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct SchoolScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
