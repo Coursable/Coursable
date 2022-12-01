281030f46e6b882de780357ad3e8b214e217434c
Ari Reitman <a.reitman@icloud.com>
2022-11-30 21:15:29 -0500

    Added new ui

---
+++ Coursable/Views/MainViews/SettingsView/AddSubjectView/AddSubjectTopBarView.swift
@@ -0,0 +1,42 @@
+//
+//  AddSubjectTopBarView.swift
+//  Coursable
+//
+//  Created by Ari Reitman on 11/30/22.
+//
+
+import SwiftUI
+
+struct AddSubjectTopBarView: View {
+    @Binding var showAddSubjectSheet: Bool
+    
+    var body: some View {
+        ZStack {
+            HStack {
+                Spacer()
+                Text("Add Subject")
+                    .foregroundColor(.white)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                Spacer()
+            }
+            
+            Button {
+                showAddSubjectSheet.toggle()
+            } label: {
+                Text("Cancel")
+                    .font(.subheadline)
+            }
+            .foregroundColor(.white)
+            .fontWeight(.semibold)
+            .offset(x: 155)
+            
+        }
+    }
+}
+
+struct AddSubjectTopBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddSubjectTopBarView(showAddSubjectSheet: .constant(false))
+    }
+}
