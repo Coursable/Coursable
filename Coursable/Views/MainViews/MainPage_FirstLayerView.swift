6bfc18a8a72c914b135e5a34cffd7cdeb1eaa4ac
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2023-01-15 20:05:17 -0500

    added code efficiency

--- Coursable/Views/MainViews/MainPage_FirstLayerView.swift
+++ Coursable/Views/MainViews/MainPage_FirstLayerView.swift
@@ -77,6 +77,7 @@ struct MainPage_FirstLayerView: View {
 
 
             ProgressRingView()
+                
 
             Spacer()
             
--- Coursable/Views/ProgressRingViews/ProgressRingView.swift
+++ Coursable/Views/ProgressRingViews/ProgressRingView.swift
@@ -23,3 +23,32 @@ struct ProgressRingView_Previews: PreviewProvider {
             .environmentObject(PeriodViewModel())
     }
 }
+
+private struct ProgressRing: View {
+    @EnvironmentObject var periodViewModel: PeriodViewModel
+    
+    var progress: Double
+    var ringColor: LinearGradient = .bluePink
+    var showMoveAnimation: Bool
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 20.0)
+                .opacity(0.3)
+                .foregroundStyle(.gray)
+            Circle()
+                .trim(from: 0.0, to: CGFloat(min(progress, 1.0)))
+                .stroke(style: StrokeStyle(lineWidth: 20.0, lineCap: .round, lineJoin: .round))
+                .foregroundStyle(ringColor)
+                .rotationEffect(Angle(degrees: 270.0))
+                .animation(.easeOut, value: showMoveAnimation)
+            
+            VStack {
+                ProgressText()
+                    .multilineTextAlignment(.center)
+            }
+            
+        }
+    }
+}
